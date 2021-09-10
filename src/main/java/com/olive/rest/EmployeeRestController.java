5e2c5bf00e9819a4adcefb16842f424e460b674b
Ravi <Ravi@DESKTOP-5A7HVVO>
2021-09-10 19:05:26 +0530

    ECS-Employee-BootStrap_Concept

---
+++ src/main/java/com/olive/rest/EmployeeRestController.java
@@ -0,0 +1,19 @@
+package com.olive.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RefreshScope
+@RestController
+public class EmployeeRestController {
+  
+	@Value("${my.app.title}")
+	private String title;
+	
+	@GetMapping("/info")
+	public String myData() {
+		System.out.println("Title :: " + title);
+		return "Data from Properties " + title;
+	}
+}
